7d401d73274022abb270ac6b252a269cfc28e9e8
Anton Wieslander <atwieslander@gmai.com>
2020-06-01 22:21:56 +0100

    rename

---
+++ MediatR/Services/Cars/Commands/CreateCarCommand.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using Services.Models;
+using Services.Wrappers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Services.Cars.Commands
+{
+    public class CreateCarCommand : IRequestWrapper<Car> {}
+
+    public class CreateCarCommandHandler : IHandlerWrapper<CreateCarCommand, Car>
+    {
+        public Task<Response<Car>> Handle(CreateCarCommand request, CancellationToken cancellationToken)
+        {
+            if (false)
+            {
+                return Task.FromResult(Response.Fail<Car>("already exists"));
+            }
+
+            return Task.FromResult(Response.Ok(new Car { Name = "Mazda" }, "Car Created"));
+        }
+    }
+}
---
+++ MediatR/Services/Wrappers/IRequestWrapper.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Wrappers
+{
+    public interface IRequestWrapper<T> : IRequest<Response<T>>
+    {}
+    public interface IHandlerWrapper<TIn, TOut> : IRequestHandler<TIn, Response<TOut>>
+        where TIn : IRequestWrapper<TOut>
+    { }
+}
